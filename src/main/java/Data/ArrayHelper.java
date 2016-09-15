6d32b1598d7013c2e60195d3ae3c7dad574362a3
Jeremiah <jeremiah@clearidea.us>
2016-09-15 17:00:41 -0400

    updated ArrayHelper class and its test counterpart

--- src/main/java/Data/ArrayHelper.java
+++ src/main/java/Data/ArrayHelper.java
@@ -6,33 +6,118 @@ package main.java.Data;
 public class ArrayHelper
 {
     /**
-     * @param aData Generic Object array
+     * Check if Value is contained in Array
+     *
+     * @param aData Array
      * @param Value Value to be compared
      * @return boolean
      */
-    public static boolean contains(Object[] aData, Object Value )
+    public static boolean contains( boolean[] aData, boolean Value )
     {
-        for ( Object Item : aData )
+        for ( boolean Item : aData )
         {
-            if ( Item.equals(Value) )
+            if ( Item == Value )
                 return true;
         }
 
         return false;
     }
 
-    /**
-     * @param aData Generic 2D Object array
-     * @param Value Value to be compared
-     * @return boolean
-     */
-    public static boolean contains(Object[][] aData, Object Value, Object Key )
+    public static boolean contains( byte[] aData, byte Value )
+    {
+        for ( byte Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( char[] aData, char Value )
+    {
+        for ( char Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( short[] aData, short Value )
+    {
+        for ( short Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( int[] aData, int Value )
+    {
+        for ( int Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( long[] aData, long Value )
+    {
+        for ( long Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( float[] aData, float Value )
+    {
+        for ( float Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( double[] aData, double Value )
+    {
+        for ( double Item : aData )
+        {
+            if ( Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( Object[] aData, Object Value )
+    {
+        for ( Object Item : aData )
+        {
+            if ( Item.equals(Value) || Item == Value )
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean contains( Object[][] aData, Object Value, Object Key )
     {
         for ( Object[] Item : aData )
         {
-            if ( Item[0].equals(Key) )
+            if ( Item[0].equals(Key) || Item[0] == Key )
             {
-                if ( Item[1].equals(Value) )
+                if ( Item[1].equals(Value) || Item[1] == Key )
                     return true;
             }
         }
@@ -40,35 +125,467 @@ public class ArrayHelper
         return false;
     }
 
+
     /**
-     * @param aData Generic 2D Object Array
-     * @param sKey The Key
+     * @param aData Array of Collections
+     * @param Key The Key
      * @param Default
-     * @return
+     * @return mixed
      */
-    public static Object getElement( Object[][] aData, String sKey, Object Default )
+    public static Object getElement( Object[][] aData, Object Key, Object Default )
     {
         for ( Object[] Item : aData )
         {
-            if ( Item[0].equals(sKey) )
-            {
+            if ( Item[0].equals(Key) || Item[0] == Key )
                return Item[1];
-            }
         }
 
-        if ( Default != null)
+        if ( Default != null )
             return Default;
+
+        return null;
+    }
+
+
+    /**
+     * @param aData Array
+     * @param Item Object to be compared
+     * @return mixed
+     */
+    public static Integer indexOf( boolean[] aData, boolean Item )
+    {
+        Integer count = 0;
+        for ( boolean obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( byte[] aData, byte Item )
+    {
+        Integer count = 0;
+        for ( byte obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( char[] aData, char Item )
+    {
+        Integer count = 0;
+        for ( char obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( short[] aData, short Item )
+    {
+        Integer count = 0;
+        for ( short obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( int[] aData, int Item )
+    {
+        Integer count = 0;
+        for ( int obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( long[] aData, long Item )
+    {
+        Integer count = 0;
+        for ( long obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( float[] aData, float Item )
+    {
+        Integer count = 0;
+        for ( float obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
+
+        return null;
+    }
+
+    public static Integer indexOf( double[] aData, double Item )
+    {
+        Integer count = 0;
+        for ( double obj : aData )
+        {
+            if ( obj == Item )
+                return count;
+
+            count++;
+        }
 
         return null;
     }
 
     public static Object indexOf( Object[] aData, Object Item )
     {
+        Integer count = 0;
+        for ( Object obj : aData )
+        {
+            if ( obj.equals(Item) || obj == Item )
+                return count;
 
+            count++;
+        }
 
+        return null;
+    }
+
+    public static Object indexOf( Object[][] aData, Object Item )
+    {
+        Integer count = 0;
+        for ( Object[] obj : aData )
+        {
+            if ( (obj[0].equals(Item) || obj[0] == Item) || (obj[1].equals(Item) || obj[1] == Item) )
+                return count;
+
+            count++;
+        }
 
         return false;
     }
 
+    /**
+     * @param aData Object Array
+     * @param Item Object to be removed
+     * @return boolean
+     */
+    public static boolean[] remove( boolean[] aData, boolean Item )
+    {
+        Object Index = indexOf( aData, Item );
 
+        if (Index == null)
+            return null;
+
+        boolean[] aTemp = new boolean[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static byte[] remove( byte[] aData, byte Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        byte[] aTemp = new byte[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static char[] remove( char[] aData, char Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        char[] aTemp = new char[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static short[] remove( short[] aData, short Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        short[] aTemp = new short[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static int[] remove( int[] aData, int Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        int[] aTemp = new int[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static long[] remove( long[] aData, long Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        long[] aTemp = new long[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static float[] remove( float[] aData, float Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        float[] aTemp = new float[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static double[] remove( double[] aData, double Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        double[] aTemp = new double[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static String[] remove( String[] aData, String Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        String[] aTemp = new String[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+    /*
+    public static Integer[] remove( Integer[] aData, Integer Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        Integer[] aTemp = new Integer[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+    */
+
+    public static Float[] remove( Float[] aData, Float Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        Float[] aTemp = new Float[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static Double[] remove( Double[] aData, Double Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        Double[] aTemp = new Double[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static Long[] remove( Long[] aData, Long Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        Long[] aTemp = new Long[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return aTemp;
+    }
+
+    public static Object remove( Object[] aData, Object Item, Class<?> cls )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return null;
+
+        Object[] aTemp = new Object[aData.length - 1];
+        for (int i=0; i<aData.length; i++)
+        {
+            if ( i < (int)Index )
+                aTemp[i] = aData[i];
+            else if ( i > (int)Index )
+                aTemp[i-1] = aData[i];
+        }
+
+        return cls.cast(aTemp);
+    }
+
+    /**
+     * @param aData Generic array of Collections
+     * @param Item Object to be removed
+     * @return boolean
+     */
+    public static Object remove( Object[][] aData, Object Item )
+    {
+        Object Index = indexOf( aData, Item );
+
+        if (Index == null)
+            return false;
+
+        Object[][] newArray = new Object[aData.length - 1][2];
+        for (int i=0; i<newArray.length; i++)
+        {
+            if (i != (int)Index)
+            {
+                newArray[i][0] = aData[i][0];
+                newArray[i][1] = aData[i][1];
+            }
+        }
+
+        return newArray;
+    }
 }
--- src/test/main/java/Data/ArrayHelperTest.java
+++ src/test/main/java/Data/ArrayHelperTest.java
@@ -2,6 +2,7 @@ package main.java.Data;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.Date;
 
 import static org.junit.Assert.*;
@@ -30,5 +31,82 @@ public class ArrayHelperTest
         assertTrue( ArrayHelper.contains( bTest, 2 ) );
         assertTrue( ArrayHelper.contains( cTest, 2.0f ) );
         assertTrue( ArrayHelper.contains( dTest, 2.0 ) );
+    }
+
+    @Test
+    public void testGetElement()
+    {
+        String[][] aTest = { { "one", "1" }, { "two", "2" }, { "three", "3" } };
+        Object[][] bTest = { { "one", 1 }, { "two", 2 }, { "three", 3 } };
+        Object[][] cTest = { { "one", 1.0f }, { "two", 2.0f }, { "three", 3.0f } };
+        Object[][] dTest = { { "one", 1.0 }, { "two", 2.0 }, { "three", 3.0 } };
+
+        // sad
+        assertEquals( null, ArrayHelper.getElement( aTest, "five", null ) );
+        assertEquals( null, ArrayHelper.getElement( bTest, "five", null ) );
+        assertEquals( null, ArrayHelper.getElement( cTest, "five", null ) );
+        assertEquals( null, ArrayHelper.getElement( dTest, "five", null ) );
+
+        // happy
+        assertEquals( "1", ArrayHelper.getElement( aTest, "one", null ) );
+        assertEquals( 20, ArrayHelper.getElement( aTest, "five", 20 ) );
+
+        assertEquals( 1, ArrayHelper.getElement( bTest, "one", null ) );
+        assertEquals( 20, ArrayHelper.getElement( bTest, "five", 20 ) );
+
+        assertEquals( 1.0f, ArrayHelper.getElement( cTest, "one", null ) );
+        assertEquals( 20, ArrayHelper.getElement( cTest, "five", 20 ) );
+
+        assertEquals( 1.0, ArrayHelper.getElement( dTest, "one", null ) );
+        assertEquals( 20, ArrayHelper.getElement( dTest, "five", 20 ) );
+    }
+
+    @Test
+    public void testIndexOf()
+    {
+        String[] aTest = { "one", "two", "three" };
+        Integer[] bTest = { 1, 2, 3 };
+        Float[] cTest = { 1.0f, 2.0f, 3.0f };
+        Double[] dTest = { 1.0, 2.0, 3.0 };
+
+        // sad
+        assertEquals( null, ArrayHelper.indexOf( aTest, "twelve" ) );
+        assertEquals( null, ArrayHelper.indexOf( bTest, "twelve" ) );
+        assertEquals( null, ArrayHelper.indexOf( cTest, "twelve" ) );
+        assertEquals( null, ArrayHelper.indexOf( dTest, "twelve" ) );
+
+        // happy
+        assertEquals( 1, ArrayHelper.indexOf( aTest, "two" ) );
+        assertEquals( 1, ArrayHelper.indexOf( bTest, 2 ) );
+        assertEquals( 1, ArrayHelper.indexOf( cTest, 2.0f ) );
+        assertEquals( 1, ArrayHelper.indexOf( dTest, 2.0 ) );
+    }
+
+    @Test
+    public void testRemove()
+    {
+        // Test Values
+        String[] aTest = { "one", "two", "three" };
+        Integer[] bTest = { 1, 2, 3 };
+        Float[] cTest = { 1.0f, 2.0f, 3.0f };
+        Double[] dTest = { 1.0, 2.0, 3.0 };
+
+        // sad
+        assertNull( ArrayHelper.remove( aTest, "twelve" ) );
+        assertNull( ArrayHelper.remove( bTest, 12, Integer[].class ) );
+        assertNull( ArrayHelper.remove( cTest, 12.0f ) );
+        assertNull( ArrayHelper.remove( dTest, 12.0 ) );
+
+        // Remove Values
+        String[] aTestResult = ArrayHelper.remove( aTest, "two");
+        Integer[] bTestResult = (Integer[])(ArrayHelper.remove( bTest, 2, Integer[].class ) );
+        Float[] cTestResult = ArrayHelper.remove( cTest, 2.0f );
+        Double[] dTestResult = ArrayHelper.remove( dTest, 2.0 );
+
+        // happy
+        assertFalse( ArrayHelper.contains( aTestResult, "two" ) );
+        assertFalse( ArrayHelper.contains( bTestResult, 2 ) );
+        assertFalse( ArrayHelper.contains( cTestResult, 2.0f ) );
+        assertFalse( ArrayHelper.contains( dTestResult, 2.0 ) );
     }
 }
